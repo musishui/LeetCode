046ebd0854f4496cc51c76df2df114869342f3fa
任新杰 <renxinjie@daojia.com>
2018-05-30 17:16:38 +0800

    feat:回文数

---
+++ 0009_回文数/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if (x < 0) return false
+  let base = x
+  let n = 0
+  while (x > 0) {
+    n = (n * 10 + x % 10)
+    x = Math.floor(x/10)
+  }
+  return base === n
+};
