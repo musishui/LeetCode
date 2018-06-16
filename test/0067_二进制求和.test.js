8cff9c6883340c0743240cc538ec75cbd9a8c0fb
任新杰 <renxinjie@daojia.com>
2018-06-16 16:35:37 +0800

    feat:二进制求和

---
+++ test/0067_二进制求和.test.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect
+var fun = require('../src/0067_二进制求和')
+describe('0067_二进制求和', () => {
+  it('用例一', () => {
+    const input = '11'
+    const input1 = '1'
+    const output = '100'
+    expect(fun(input, input1)).to.be.equal(output)
+  })
+  it('用例二', () => {
+    const input = '1010'
+    const input1 = '1011'
+    const output = '10101'
+    expect(fun(input, input1)).to.be.equal(output)
+  })
+})
