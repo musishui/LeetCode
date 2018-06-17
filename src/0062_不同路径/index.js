60ed4058ee2a8fed99af7adc0b2d7233b6c20693
musishui <rxj_2008@foxmail.com>
2018-06-17 13:28:08 +0800

    feat:不同路径

---
+++ src/0062_不同路径/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  let sum = m + n - 2
+  n = Math.min(m, n) - 1
+  let num = 1
+  let result = 1
+  for (; n >= 1; n--) {
+    num *= n
+    result *= (sum - n + 1)
+  }
+  return result / num
+};
+uniquePaths(1, 10)
+module.exports = uniquePaths
