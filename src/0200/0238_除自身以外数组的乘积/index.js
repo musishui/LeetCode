4aa73e21a569feb5c02f5db4894e9c1fcb7d4775
rxj2008 <rxj2008@foxmail.com>
2020-12-17 21:59:36 +0800

    feat: 238

--- src/0200/0238_除自身以外数组的乘积/index.js
+++ src/0200/0238_除自身以外数组的乘积/index.js
@@ -8,10 +8,10 @@ var productExceptSelf = function (nums) {
     suffix = 1;
   let ans = new Array(len).fill(1);
   for (let i = 0; i < len; i++) {
-      ans[i] *=prefix
-      ans[len-i-1]*=suffix
-      prefix*=nums[i]
-      suffix*=nums[len-i-1]
+    ans[i] *= prefix;
+    ans[len - i - 1] *= suffix;
+    prefix *= nums[i];
+    suffix *= nums[len - i - 1];
   }
-  return ans
+  return ans;
 };
